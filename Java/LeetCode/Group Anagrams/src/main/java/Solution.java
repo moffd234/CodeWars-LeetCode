1dda7f487d8eac086bbdadeb7cd3cb04414ffa7a
Daniel Moffett <moffd234@gmail.com>
2025-07-22 16:51:50 -0400

    Added a Java and Python directory to split challenges by language

--- Java/LeetCode/Group Anagrams/src/main/java/Solution.java
+++ Java/LeetCode/Group Anagrams/src/main/java/Solution.java
@@ -1,8 +1,16 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strings) {
+        if(strings.length == 1){
+            return List.of(List.of(strings));
+        }
+
+        // Brute force solution
+
+
         return new ArrayList<List<String>>(){};
     }
 }
