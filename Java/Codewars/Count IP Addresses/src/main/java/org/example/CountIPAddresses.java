b8813510fe54fa1abb0ac8265a4c7833e31b9596
Daniel Moffett <moffd234@gmail.com>
2026-02-28 13:42:08 -0500

    Finished Count IP Addresses

--- Java/Codewars/Count IP Addresses/src/main/java/org/example/CountIPAddresses.java
+++ Java/Codewars/Count IP Addresses/src/main/java/org/example/CountIPAddresses.java
@@ -3,6 +3,17 @@ package org.example;
 public class CountIPAddresses {
 
     public static long ipsBetween(String start, String end) {
-        return -1;
+        return convertIpToDecimalLong(end) - convertIpToDecimalLong(start);
+    }
+
+    private static long convertIpToDecimalLong(String ip){
+        String[] octets = ip.split("\\.");
+        long decimalIp = 0;
+
+        for(String octet : octets){
+            decimalIp = decimalIp * 256 + Long.parseLong(octet);
+        }
+
+        return decimalIp;
     }
 }
