1e77888c96bf1a3135035fc342f8eaebbcafe377
Daniel Moffett <moffd234@gmail.com>
2026-02-28 13:26:04 -0500

    Added base files for Count IP Addresses

---
+++ Java/Codewars/Count IP Addresses/src/main/java/org/example/CountIPAddresses.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class CountIPAddresses {
+
+    public static long ipsBetween(String start, String end) {
+        return -1;
+    }
+}
---
+++ Java/Codewars/Count IP Addresses/src/test/java/CountIPAddressesTest.java
@@ -0,0 +1,21 @@
+import org.example.CountIPAddresses;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CountIPAddressesTest {
+    private void doTest(long expected, String start, String end) {
+        long actual = CountIPAddresses.ipsBetween(start, end);
+        String message = String.format("for start = \"%s\" and end = \"%s\"", start, end);
+        assertEquals(expected, actual, message);
+    }
+
+    @Test
+    public void sampleTests() {
+        doTest( 50, "10.0.0.0", "10.0.0.50");
+        doTest( 246, "20.0.0.10", "20.0.1.0");
+        doTest( (1l << 32l) - 1l, "0.0.0.0", "255.255.255.255");
+        doTest( 1, "150.0.0.0", "150.0.0.1");
+        doTest( 50, "10.0.0.0", "10.0.0.50");
+        doTest( 246, "20.0.0.10", "20.0.1.0");
+    }
+}
