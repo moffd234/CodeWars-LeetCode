e0101f415251ca56998553bc61c4645c1192d6c6
Daniel Moffett <moffd234@gmail.com>
2026-03-01 23:42:12 -0500

    Added starter code for Design Dynamic Array

---
+++ Java/NeetCode/Design Dynamic Array/src/main/java/org/example/Main.java
@@ -0,0 +1,36 @@
+package org.example;
+
+class DynamicArray {
+
+    public DynamicArray(int capacity) {
+
+    }
+
+    public int get(int i) {
+        return 0;
+    }
+
+    public void set(int i, int n) {
+
+    }
+
+    public void pushback(int n) {
+
+    }
+
+    public int popback() {
+        return 0;
+    }
+
+    private void resize() {
+
+    }
+
+    public int getSize() {
+        return 0;
+    }
+
+    public int getCapacity() {
+        return 0;
+    }
+}
