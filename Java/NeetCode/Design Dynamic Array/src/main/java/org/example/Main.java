651dfa03a90ac60966f13d08bfcdfea6189e380f
Daniel Moffett <moffd234@gmail.com>
2026-03-01 23:48:12 -0500

    Wrote get and set

--- Java/NeetCode/Design Dynamic Array/src/main/java/org/example/Main.java
+++ Java/NeetCode/Design Dynamic Array/src/main/java/org/example/Main.java
@@ -1,5 +1,17 @@
 package org.example;
 
+/**
+DynamicArray(int capacity) will initialize an empty array with a capacity of capacity, where capacity > 0.
+int get(int i) will return the element at index i. Assume that index i is valid.
+void set(int i, int n) will set the element at index i to n. Assume that index i is valid.
+void pushback(int n) will push the element n to the end of the array.
+int popback() will pop and return the element at the end of the array. Assume that the array is non-empty.
+void resize() will double the capacity of the array.
+int getSize() will return the number of elements in the array.
+int getCapacity() will return the capacity of the array.
+ */
+
+
 class DynamicArray {
     int[] array;
     int capacity;
@@ -10,11 +22,11 @@ class DynamicArray {
     }
 
     public int get(int i) {
-        return 0;
+        return array[i];
     }
 
     public void set(int i, int n) {
-
+        array[i] = n;
     }
 
     public void pushback(int n) {
