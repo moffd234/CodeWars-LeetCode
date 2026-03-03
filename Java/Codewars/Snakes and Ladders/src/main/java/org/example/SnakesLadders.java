8e5449107cd7b36e3d361109e81f8b4cc636ffb8
Daniel Moffett <moffd234@gmail.com>
2026-03-02 20:06:39 -0500

    Added Player class as an inner class due to CodeWars constraints

--- Java/Codewars/Snakes and Ladders/src/main/java/org/example/SnakesLadders.java
+++ Java/Codewars/Snakes and Ladders/src/main/java/org/example/SnakesLadders.java
@@ -85,4 +85,28 @@ public class SnakesLadders {
     public Player getPlayer2() {
         return player2;
     }
+
+
+    public static class Player {
+        private int pos = 0;
+
+        public int move(int num) {
+            pos += num;
+
+            if (pos > 100) {
+                pos = 100 - (pos - 100);
+            }
+
+            return pos;
+        }
+
+        public int getPos() {
+            return pos;
+        }
+
+        public void setPos(int pos) {
+            this.pos = pos;
+        }
+    }
 }
+
