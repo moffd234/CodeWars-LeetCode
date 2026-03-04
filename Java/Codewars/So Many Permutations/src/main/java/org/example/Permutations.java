e8b06bd1820d300c25dd8d65a5a88c65e1ef9e01
Daniel Moffett <moffd234@gmail.com>
2026-03-03 20:53:46 -0500

    Added starter files for So Many Permutations

---
+++ Java/Codewars/So Many Permutations/src/main/java/org/example/Permutations.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.List;
+
+public class Permutations {
+
+    public static List<String> singlePermutations(String s) {
+        // Your code here!
+        return null;
+    }
+}
