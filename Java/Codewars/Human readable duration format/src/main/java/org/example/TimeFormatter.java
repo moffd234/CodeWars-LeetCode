367f142838bdaba10671444f0fcb3852e4c21cfd
Daniel Moffett <moffd234@gmail.com>
2026-03-04 17:41:08 -0500

    Wrote formatOutput

--- Java/Codewars/Human readable duration format/src/main/java/org/example/TimeFormatter.java
+++ Java/Codewars/Human readable duration format/src/main/java/org/example/TimeFormatter.java
@@ -9,6 +9,28 @@ public class TimeFormatter {
         return null;
     }
 
+    public static String formatOutput(int hours, int minutes, int seconds){
+        ArrayList<String> timeList = new ArrayList<>();
+
+        appendTime(timeList, hours, "hour");
+        appendTime(timeList, minutes, "minute");
+        appendTime(timeList, seconds, "second");
+
+        if(timeList.size() == 1){
+            return timeList.get(0);
+        }
+
+        if(timeList.size() == 2){
+            return timeList.get(0) + " and " + timeList.get(1);
+        }
+
+        if(timeList.size() == 3){
+            return timeList.get(0) + ", " + timeList.get(1) + " and " + timeList.get(2);
+        }
+
+        return null;
+    }
+
     public static void appendTime(ArrayList<String> timeList, int timeVal, String timeType){
         if(timeVal == 0){
             return;
