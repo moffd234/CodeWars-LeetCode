66a71e5b11f317d829f5650ba899eb7c72b7b9d0
Daniel Moffett <moffd234@gmail.com>
2026-03-04 17:51:50 -0500

    Added year and day support

--- Java/Codewars/Human readable duration format/src/main/java/org/example/TimeFormatter.java
+++ Java/Codewars/Human readable duration format/src/main/java/org/example/TimeFormatter.java
@@ -5,38 +5,45 @@ import java.util.ArrayList;
 public class TimeFormatter {
 
     public static String formatDuration(int seconds) {
+        if(seconds == 0){
+            return "now";
+        }
+
         final int SECONDS_IN_MINUTE = 60;
         final int SECONDS_IN_HOUR = 3600;
+        final int SECONDS_IN_DAY = 86400;
+        final int SECONDS_IN_YEARS = 31536000;
 
-        int hours = seconds / SECONDS_IN_HOUR;
-        int remainder = seconds % SECONDS_IN_HOUR;
+        int years = seconds / SECONDS_IN_YEARS;
+        int remainder = seconds % SECONDS_IN_YEARS;
+
+        int days = remainder / SECONDS_IN_DAY;
+        remainder = remainder % SECONDS_IN_DAY;
+
+        int hours = remainder / SECONDS_IN_HOUR;
+        remainder = seconds % SECONDS_IN_HOUR;
 
         int minutes = remainder / SECONDS_IN_MINUTE;
         remainder = remainder % SECONDS_IN_MINUTE;
 
-        return formatOutput(hours, minutes, remainder);
+        return formatOutput(years, days, hours, minutes, remainder);
     }
 
-    public static String formatOutput(int hours, int minutes, int seconds){
+    public static String formatOutput(int years, int days, int hours, int minutes, int seconds){
         ArrayList<String> timeList = new ArrayList<>();
 
+        appendTime(timeList, years, "year");
+        appendTime(timeList, days, "day");
         appendTime(timeList, hours, "hour");
         appendTime(timeList, minutes, "minute");
         appendTime(timeList, seconds, "second");
 
-        if(timeList.size() == 1){
-            return timeList.get(0);
-        }
+        if (timeList.size() == 1) return timeList.get(0);
+        if (timeList.size() == 2) return timeList.get(0) + " and " + timeList.get(1);
 
-        if(timeList.size() == 2){
-            return timeList.get(0) + " and " + timeList.get(1);
-        }
-
-        if(timeList.size() == 3){
-            return timeList.get(0) + ", " + timeList.get(1) + " and " + timeList.get(2);
-        }
-
-        return null;
+        return String.join(", ", timeList.subList(0, timeList.size() - 1))
+                + " and "
+                + timeList.get(timeList.size() - 1);
     }
 
     public static void appendTime(ArrayList<String> timeList, int timeVal, String timeType){
