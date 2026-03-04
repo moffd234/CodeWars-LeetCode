99f43e39ab0056bdaa802cd12db554b3b21173b4
Daniel Moffett <moffd234@gmail.com>
2026-03-04 17:39:09 -0500

    Wrote appendTime

--- Java/Codewars/Human readable duration format/src/main/java/org/example/TimeFormatter.java
+++ Java/Codewars/Human readable duration format/src/main/java/org/example/TimeFormatter.java
@@ -1,9 +1,19 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class TimeFormatter {
 
     public static String formatDuration(int seconds) {
         // your code goes here
         return null;
+    }
+
+    public static void appendTime(ArrayList<String> timeList, int timeVal, String timeType){
+        if(timeVal == 0){
+            return;
+        }
+
+        timeList.add(timeVal + " " + timeType + (timeVal > 1 ? "s" : ""));
     }
 }
